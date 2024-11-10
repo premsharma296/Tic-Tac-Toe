e069822bad233267e0796f68b028994bd6d1ca34
Premchand Sharma <120751390+premsharma296@users.noreply.github.com>
2024-11-10 12:14:39 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+let newbtn=document.createElement("button");
+newbtn.innerText="click me!";
+console.log(newbtn);
+
+let div=document.querySelector("div");
+div.prepend(newbtn);
+
+
---
+++ pp.py
@@ -0,0 +1,29 @@
+# COMPREHENSION
+
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+# for index,number in enumerate(numbers):
+#     print(index,number)
+
+#     lst=[]
+#     for x in range(10):
+#         lst.append(x**3)
+#         print(lst)
+
+# num=[x**2 for x in range(10)]
+# print(num)
+
+# lst=[]
+# for x in range(10):
+#     if x%2==0:
+#         lst.append(x)
+        
+#         print(lst)
+
+# x=1
+
+# for x in range(10):
+#     x=x+1
+#     print(x)
+
+k=set(range(1,10))
+print(k)
